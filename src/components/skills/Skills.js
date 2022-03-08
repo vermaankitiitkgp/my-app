9404105cfcbb46347720ad2fbb3bb3b2d2bb27d6
vermaankitiitkgp <vermaankit.iitkgp@gmail.com>
2022-03-09 00:19:06 +0530

    with use selector

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -8,44 +8,60 @@ import { createSkills, updateSkills, deleteSkills } from "./skillsSlice";
 
 export default function Skills() {
     const [data, setData] = useState({ name: "", age: "" });
-    const [inputFields, setInputFields] = useState(useSelector((state) => state.skills.value));
+    const inputFields = useSelector((state) => state.skills.value);
     const dispatch = useDispatch();
-    const handleFormChange = (index, event) => {
+    console.log(inputFields);
+    let [dirty, setDirty] = useState({});
+    const handleFormChange = (index, e) => {
 
-        let data = inputFields.map((row, rowIndex) => {
+        // let data = inputFields.map((row, rowIndex) => {
 
-            if (index == rowIndex) {
-                return {
-                    ...row,
-                    [event.target.name]: event.target.value
+        //     if (index == rowIndex) {
+        //         return {
+        //             ...row,
+        //             [event.target.name]: event.target.value
+        //         }
+        //     }
+        //     return row;
+        // })
+        if (!dirty[index]) {
+            setDirty({
+                [index]: {
+                    [e.target.name]: e.target.value
                 }
-            }
-            return row;
-        })
-        console.log(data);
-        setInputFields(data);
+            })
+        }
+        else {
+            let k = { ...dirty, ...{ [index]: { ...dirty[index], ...{ [e.target.name]: e.target.value } } } };
+            setDirty(k);
+        }
+
+        console.log(dirty);
+        //setInputFields(data);
     }
     const saveField = (e) => {
         e.preventDefault();
         dispatch(createSkills(data));
-        setInputFields([...inputFields, data]);
+        //setInputFields([...inputFields, data]);
         setData({});
     }
     const updateField = (index, newValue, e) => {
         e.preventDefault();
         console.log(newValue);
         dispatch(updateSkills({ index, newValue }));
+        setDirty({});
     }
     const deleteField = (index, e) => {
         e.preventDefault();
         dispatch(deleteSkills(index));
-        let cp_inputs = [...inputFields];
-        cp_inputs.splice(index, 1);
-        setInputFields(cp_inputs);
+        //let cp_inputs = [...inputFields];
+        //cp_inputs.splice(index, 1);
+        //setInputFields(cp_inputs);
     }
 
     return (
         <div>
+            <h1>Skills</h1>
             <form>
                 {inputFields.map((input, index) => {
                     return (
@@ -53,17 +69,17 @@ export default function Skills() {
                             <input
                                 name='name'
                                 placeholder='Skill'
-                                value={input.name}
+                                value={(dirty[index] && dirty[index].name) || input.name}
                                 onChange={event => handleFormChange(index, event)}
                             />
                             <input
                                 name='age'
                                 placeholder='Expertise Level'
-                                value={input.age}
+                                value={(dirty[index] && dirty[index].age) || input.age}
                                 onChange={event => handleFormChange(index, event)}
                             />
 
-                            <button onClick={(event) => updateField(index, input, event)}>Update</button>
+                            <button type="button" onClick={(e) => updateField(index, dirty[index], e)}>Update</button>
                             <button onClick={(event) => deleteField(index, event)}>Delete</button>
                         </div>
                     )
